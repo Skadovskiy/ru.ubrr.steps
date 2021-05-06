6862726cade1d652e1bdd2e8d58718911e8481a2
Skadovskiy <skadovskiy@ubrr.ru>
2021-05-06 21:54:47 +0500

    step3. Использованеи jitpack.io в качестве репозитория

---
+++ src/main/java/games/EmptytestclassImpl.java
@@ -0,0 +1,8 @@
+package games;
+
+//Класс Emptytestclass импортирован из https://jitci.com/gh/Skadovskiy/EmptyProject
+import ru.ubrr.emptyproject.Emptytestclass;
+
+public class EmptytestclassImpl extends Emptytestclass {
+
+}
